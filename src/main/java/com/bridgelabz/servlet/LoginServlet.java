1e79dffa5e364221b467c1343bb89fbc5cb2ca3d
nitishgupta978 <nitishgupta9785@gmail.com>
2022-05-27 20:25:28 +0530

    Using Regex Pattern Validation

--- src/main/java/com/bridgelabz/servlet/LoginServlet.java
+++ src/main/java/com/bridgelabz/servlet/LoginServlet.java
@@ -1,15 +1,78 @@
 package com.bridgelabz.servlet;
-//@WebServlet(
-//        description = "login Servlet Testing",
-//        urlPatterns = {"/loginServlet"},
-//        initParams = {
-//                @WebInitParam(name="user", value="John"),
-//                @WebInitParam(name = "password",value="BridgeLabz")
-//        }
-//
-//public class LoginServlet extend HttpServlet {
-//                @Override
-//        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  ServletException,IOException {
-//                        String user =
-//        }
-//}
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(
+        description = "Login Servlet Testing",
+        urlPatterns = {"/LoginServlet"},
+        initParams = {
+                @WebInitParam(name = "user", value = "John"),
+                @WebInitParam(name = "password", value = "john@123")
+        }
+)
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //get request parameters for userID and password
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
+        //get servlet config init params
+        String userID = getServletConfig().getInitParameter("user");
+        String password = getServletConfig().getInitParameter("password");
+        //regex for username and password
+        String userRegex = "^[A-Z]{1}.{2,}$";//username regex
+        /*
+        Checking for user name regex
+        Atleast one upper case
+        min 3 characters
+         */
+        if (!user.matches(userRegex)) {
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Kindly Enter Correct user name</font>");
+        }
+
+         /*
+            ^ represents starting character of the string.
+            (?=.*[0-9]) represents a digit must occur at least once.
+            (?=.*[a-z]) represents a lower case alphabet must occur at least once.
+            (?=.*[A-Z]) represents an upper case alphabet that must occur at least once.
+            (?=.*[@#$%^&-+=()] represents a special character that must occur at least once.
+            (?=\\S+$) white spaces don’t allowed in the entire string.
+            .{8, 20} represents at least 8 characters and at most 20 characters.
+            $ represents the end of the string.
+             */
+        String passwordRegex = "^(?=.*[0-9])"
+                + "(?=.*[a-z])(?=.*[A-Z])"
+                + "(?=.*[@#$%^&+=])"
+                + "(?=\\S+$).{8,20}$";
+        /*
+        Checking for password regex
+         */
+        if (!pwd.matches(passwordRegex)) {
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Kindly Enter Correct password</font>");
+        }
+
+
+        if (userID.equals(user) && password.equals(pwd)) {
+            request.setAttribute("user", user);
+            request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            if (!userID.equals(user))
+                out.println("<font color=red>  user name is wrong.</font>");
+            if (!password.equals(pwd))
+                out.println("<font color=red>  password is wrong.</font>");
+            rd.include(request, response);
+        }
+    }
+}
